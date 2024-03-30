ce89595c84d187c8627c253d6cdc38dac36adb50
Mahmoud Aboheif <mahmoudabouheif575@gmail.com>
2024-03-31 01:43:16 +0200

    Adding Client Personal info

---
+++ Reservation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Management_System
+{
+    internal class Reservation
+    {
+        protected string firstname;
+        protected string secondname;
+        protected string phone_number;
+        protected int room_number;
+        protected bool occupancy;
+        //protected List<Reservation> reservations;
+
+        // Creating Properties
+ /*--------------------------------------------------------------------------------*/   
+        
+        public string FirstName
+        {
+            get { return firstname; }
+            set { firstname = value; }
+        }
+
+        public string SecondName
+        {
+            get { return secondname; }
+            set { secondname = value; }
+        }
+
+        public string PhoneNumber
+        {
+            get { return phone_number; }
+
+            set
+            {    if(value.Length == 11) //Cheacking the Length of the Phone Number ic correct
+                    phone_number = value;
+            }
+
+        }
+
+        public int RoomNumber
+        {
+            get { return room_number; }
+            set
+            {   if (value > 0 || value < 1000)
+                    room_number = value;
+            }
+        }
+
+        public bool Occupancy
+        {
+            get { return occupancy; }
+            set { occupancy = value; }
+        }
+
+
+    }//End of Class
+
+}// End of Program
