7e151931c2f006df80803f7e6bbee8e052cfe0d7
MahmoudAboheif <146019503+MahmoudAboheif@users.noreply.github.com>
2024-03-30 20:53:27 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hotel_Management_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
